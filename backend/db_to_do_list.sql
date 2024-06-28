5570aaa35dc0aeb900afcc5c8edf265a4c4e1806
Gabriel Céspedes Ugalde <141288782+gcesu@users.noreply.github.com>
2024-06-27 20:48:30 -0600

    codigo actual

--- backend/db_to_do_list.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS tasks (
-    id INTEGER PRIMARY KEY (1,1),
-    name TEXT NOT NULL,
-    date TEXT NOT NULL,
-    time TEXT NOT NULL,
-    description TEXT,
-    day TEXT NOT NULL
-);
